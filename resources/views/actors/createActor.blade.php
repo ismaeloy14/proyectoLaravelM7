4c36035ccd1cccae6cb8a2d13e7ec4ff000b3ec8
Alex Freixa <alexfreixa18@gmail.com>
2019-02-20 15:49:55 +0100

    Views create i edit actor afegides

---
+++ resources/views/actors/createActor.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="row" style="margin-top:40px">
+   <div class="offset-md-3 col-md-6">
+      <div class="card">
+         <div class="card-header text-center">
+            Añadir actor
+         </div>
+         <div class="card-body" style="padding:30px">
+
+            <form method="POST" action="{{url('/actor/create')}}">
+
+	            {{ csrf_field() }}
+	            
+					<div class="form-group">
+	               <label for="nom_actor">Nombre</label>
+	               <input type="text" name="nom_actor" id="nom_actor" class="form-control">
+	            </div>
+
+	            <div class="form-group">
+	               <label for="nacionalitat">Nacionalidad</label>
+	               <input type="text" name="nacionalitat" id="nacionalitat" class="form-control">
+	            </div>
+
+	            <div class="form-group">
+	               <label for="retrato">Retrato</label>
+	               <input type="text" name="retrato" id="retrato" class="form-control">
+	            </div>
+
+	            <div class="form-group text-center">
+	               <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+	                   Añadir actor
+	               </button>
+	            </div>
+					
+        	</form>
+         </div>
+      </div>
+   </div>
+</div>
+
+
+    
+
+@stop
---
+++ resources/views/actors/editActor.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="row" style="margin-top:40px">
+   <div class="offset-md-3 col-md-6">
+      <div class="card">
+         <div class="card-header text-center">
+            Modificar actor
+         </div>
+         <div class="card-body" style="padding:30px">
+
+            <form method="POST" action="{{url('/actor/edit/'.$actor->id)}}">
+
+            	{{method_field('PUT')}}
+
+	            {{ csrf_field() }}
+
+	            <div class="form-group">
+	               <label for="nom_actor">Nombre</label>
+	               <input type="text" name="nom_actor" id="nom_actor" class="form-control" value="{{$actor->nom_actor}}">
+	            </div>
+
+	            <div class="form-group">
+	               <label for="nacionalitat">Nacionalidad</label>
+	               <input type="text" name="nacionalitat" id="nacionalitat" class="form-control" value="{{$actor->nacionalitat}}">
+	            </div>
+
+	            <div class="form-group">
+	               <label for="retrato">Retrato</label>
+	               <input type="text" name="retrato" id="retrato" class="form-control" value="{{$actor->retrato}}">
+	            </div>
+
+
+	            <div class="form-group text-center">
+	               <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+	                   Modificar película
+	               </button>
+	            </div>
+
+	            {{-- TODO: Cerrar formulario --}}
+        	</form>
+         </div>
+      </div>
+   </div>
+</div>
+@stop
