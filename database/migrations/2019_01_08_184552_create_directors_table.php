64201f6221e8e8e048ff434ce27e526921344f03
Ismael Angulo <ismaeloy14@gmail.com>
2019-02-20 16:49:54 +0100

    se ha modificado la tabla directores añadiendo el campo imagendirector segunda parte

--- database/migrations/2019_01_08_184552_create_directors_table.php
+++ database/migrations/2019_01_08_184552_create_directors_table.php
@@ -17,6 +17,7 @@ class CreateDirectorsTable extends Migration
             $table->string('nom_director', 64)->primary();
             $table->string('nacionalitat');
             $table->date('data_naixement');
+            $table->string('imagenDirector')->nullable();
         });
     }
 
